4dbe4627780edee81cf407f610dc9b39a243eea0
2gg111222 <124110995+2gg111222@users.noreply.github.com>
2023-04-14 23:45:35 +0900

    Add files via upload

---
+++ ds05/src/문자열/함수만들기연습2.java
@@ -0,0 +1,21 @@
+package 문자열;
+
+public class 함수만들기연습2 {
+	
+	public static void 운동하다() {
+		System.out.println("운동1) 웨이트운동하다.");
+		System.out.println("운동2) 유산소운동하하다.");
+		System.out.println("운동3) 집에 간다.");
+	}
+	
+	public static void 공부하다() {
+		System.out.println("자바1) 자바배열공부하다.");
+		System.out.println("자바2) 알고리즘공부하다.");
+		System.out.println("자바3) 프로그래머스문제풀다.");
+	}
+	
+	public static void main(String[] args) {
+		공부하다();
+		운동하다();
+	}
+}
